72e24fa27b39a79d2fb076934a249d1b8b8f02a3
Gu Zhigang <gzg@centos.gzg.yh>
2014-11-17 08:24:12 +0800

    sample files

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-var app=angular.module("gSudoku-test",["gSudoku"]);
+var app=angular.module("gSudoku-test",["ngRoute","gSudoku"]);
+app.config(["$routeProvider",function($routeProvider){
+	$routeProvider.when("/",{controller:"mainCtrl",templateUrl:"menu.html"})
+	.when("/cell7",{controller:"cell7Ctrl",templateUrl:"cell7.html"})
+	.when("/cell6",{controller:"cell6Ctrl",templateUrl:"cell6.html"})
+	.otherwise({redirectTo:"/"});
+}]);
+
+app.controller("mainCtrl",function($scope){
+});
+
+app.controller("cell6Ctrl",function($scope){
+});
+
+app.controller("cell7Ctrl",function($scope){
+});
